d56e8865e950131d07f211f9399395644f5ac546
Turandor <senjin56@gmail.com>
2020-08-11 13:35:50 +0200

    Finished algorithm + prime number generator

---
+++ PrimeNumberRecognizer/Number.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeNumberRecognizer
+{
+    public static class PrimeNumberRecognizer
+    {
+        public static bool IsPrime(int value)
+        {
+            if (value > 1)
+            {
+                float sqrt = (float)Math.Sqrt(value);
+                for (int i = 2; i <= sqrt; i++)
+                {
+                    if (value % i == 0)
+                        return false;
+                }
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
---
+++ PrimeNumberRecognizer/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace PrimeNumberRecognizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool quit = false;
+            bool decision = false;
+            string key;
+
+            Console.WriteLine("Hello! type your number and check if it is prime");
+            while (!quit)
+            { 
+                int value = int.Parse(Console.ReadLine());
+
+                if (PrimeNumberRecognizer.IsPrime(value))
+                    Console.WriteLine("Number is prime");
+                else
+                    Console.WriteLine("Number is not prime");
+            
+                while (!decision)
+                {
+                    Console.WriteLine("Want to try again? y/n");
+                    key = Console.ReadLine();
+                    if (key == "n")
+                        quit = decision = true;
+                    else if (key == "y")
+                    {
+                        decision = true;
+                    }
+                    else
+                        Console.WriteLine("Wrong command!");
+                }
+                decision = false;
+            }
+
+            // prime number generator
+            /*
+            int a = 2;
+            Console.WriteLine("List of prime numbers");
+            while (true)
+            {
+                if (PrimeNumberRecognizer.IsPrime(a))
+                    Console.WriteLine(a);
+                a++;
+            }
+            */
+        }
+    }
+}
